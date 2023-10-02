688bb0f8ffe586b50c96aa8bad3edacc2b897740
ShakirSuleimanov <suleymanov-ef@mail.ru>
2023-10-02 20:33:45 +0300

    Add annotations to nucleic_acids_tools.py

--- source/nucleic_acids_tools.py
+++ source/nucleic_acids_tools.py
@@ -2,38 +2,29 @@ import source.nucleic_acids_dict as dict
 
 
 # Check if seq is DNA
-def is_dna(seq):
+def is_dna(seq: str) -> bool:
     unique_chars = set(seq)
     nucleotides = set('AaTtGgCc')
     return unique_chars <= nucleotides
 
 
 # Check if seq is RNA
-def is_rna(seq):
+def is_rna(seq: str) -> bool:
     unique_chars = set(seq)
     nucleotides = set('AaUuGgCc')
     return unique_chars <= nucleotides
 
 
 # Transcription function
-def transcribe(seq):
+def transcribe(seq: str) -> str:
     if not is_dna(seq):
         raise ValueError("Sequence is not a DNA, input should be DNA")
     else:
         return ''.join([dict.TRANSCRIPTION_DICT[i] for i in seq])
 
 
-# Function for reverse seq
-#def reverse(seq):
-    if is_dna(seq) or is_rna(seq) is True:
-        reverse_seq = seq[::-1]
-        return reverse_seq
-    else:
-        raise ValueError("Sequence is not a DNA/RNA, input should be DNA/RNA")
-
-
 # Reverse function
-def reverse(seq):
+def reverse(seq: str) -> str:
     if not is_dna(seq) or is_rna(seq):
         raise ValueError("Sequence is not a DNA/RNA, input should be DNA/RNA")
     else:
@@ -41,7 +32,7 @@ def reverse(seq):
 
 
 # Function for reverse seq
-def complement(seq):
+def complement(seq: str) -> str:
     if not is_dna(seq):
         raise ValueError("Sequence is not a DNA, input should be DNA")
     else:
@@ -49,7 +40,7 @@ def complement(seq):
          
 
 # Function for make reverse and complement seq
-def reverse_complement(seq):
+def reverse_complement(seq: str) -> str:
     if not is_dna(seq):
         raise ValueError("Sequence is not a DNA, input should be DNA")
     else:
@@ -57,7 +48,7 @@ def reverse_complement(seq):
 
 
 # Function for rewriting nucleotide sequence to binary code
-def make_binary(seq):
+def make_binary(seq:str) -> tuple:
     if not is_dna(seq) or is_rna(seq):
         raise ValueError("Sequence is not a DNA/RNA, input should be DNA/RNA")
     else:

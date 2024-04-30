c92807797de045e01e68e5d9163256c6500dc2b2
ShakirSuleimanov <suleymanov-ef@mail.ru>
2024-04-30 21:26:22 +0300

    Start working on multiprocessing the random forest

--- custom_random_forest.py
+++ custom_random_forest.py
@@ -1,6 +1,11 @@
 import numpy as np
+from concurrent.futures import ProcessPoolExecutor
 from sklearn.base import BaseEstimator
 from sklearn.tree import DecisionTreeClassifier
+from joblib import dump, load
+import os
+import multiprocessing
+
 
 class RandomForestClassifierCustom(BaseEstimator):
     def __init__(self, n_estimators=10, max_depth=None, max_features=None, random_state=None):
@@ -8,27 +13,49 @@ class RandomForestClassifierCustom(BaseEstimator):
         self.max_depth = max_depth
         self.max_features = max_features
         self.random_state = random_state
-
+        
+        self.bootstrapped_data = []
         self.trees = []
         self.feat_ids_by_tree = []
         self.classes_ = None
+    
+    def fit_tree(X, y):
+        current_tree_model = DecisionTreeClassifier(max_depth=self.max_depth, random_state=self.random_state)
+        current_tree = current_tree_model.fit(X, y)
+        #self.trees.append(current_tree)
+        return current_tree
 
-    def fit(self, X, y):
+    def fit(self, X, y, n_jobs=1):
+        '''
+        Function to fit the random forest to a training set.
+        '''
         self.classes_ = sorted(np.unique(y))
 
         for i in range(0, self.n_estimators):
             np.random.seed(self.random_state + i)
-            feature_idx = np.array(np.random.choice(X.shape[1], self.max_features, replace = False))
+            feature_idx = np.array(np.random.choice(X.shape[1], self.max_features, replace=False))
             self.feat_ids_by_tree.append(feature_idx)
             bootstrapped_idx = np.array(np.random.choice(X.shape[0], X.shape[0], replace=True))
             bootstrapped_X = np.array([X[i] for i in bootstrapped_idx])
             bootstrapped_X_with_selected_features = np.array([bootstrapped_X[:, k] for k in self.feat_ids_by_tree[i]]).T
             bootstrapped_y = np.array([y[i] for i in bootstrapped_idx])
-            current_tree_model = DecisionTreeClassifier(max_depth=self.max_depth, random_state=self.random_state)
-            current_tree = current_tree_model.fit(bootstrapped_X_with_selected_features, bootstrapped_y)
-            self.trees.append(current_tree)
+            bootstrapped_pair = (bootstrapped_X_with_selected_features, bootstrapped_y)
+            self.bootstrapped_data.append(bootstrapped_pair)
+        
+        with ProcessPoolExecutor(10) as pool:
+            results = list(pool.map(self.fit_tree, self.bootstrapped_data))
+        
+        self.trees.append(results)
         return self
+            #dump(current_tree, f'{os.getcwd()}/my_randomforest_model_{pid}.joblib') 
 
+        #processes = [multiprocessing.Process(target=fit_tree, args=(data, pid)) for pid, data in enumerate(self.bootstrapped_data)]
+        #for proc in processes:
+        #    proc.start()
+        #for proc in processes:
+        #    proc.join()
+        #return results
+        return self
     def predict_proba(self, X):
         '''
         Function to predict probabilities of the class using the random forest.
@@ -37,7 +64,7 @@ class RandomForestClassifierCustom(BaseEstimator):
         for i in range(len(self.trees)):
             y_pred = self.trees[i].predict_proba(np.array([X[:, k] for k in self.feat_ids_by_tree[i]]).T)
             total_pred.append(y_pred)
-        return np.mean(total_pred, axis = 0)
+        return np.mean(total_pred, axis=0)
 
     def predict(self, X):
         '''
